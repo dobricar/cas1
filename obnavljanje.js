c0c23d11247e8721cb9c4d6dfac9166f9a193421
Milan Dobrijevic <dobrijevic_milan@yahoo.com>
2020-02-03 13:14:15 +0100

    novo

---
+++ obnavljanje.js
@@ -0,0 +1,100 @@
+
+/*let x = 6;
+switch(x){
+    case 0:
+            console.log('radan dan');
+        break;
+    case 1:
+            console.log('radan dan');
+        break;
+    case 2:     
+            console.log('radan dan');
+        break;
+        case 3:     
+            console.log('radan dan')
+        break;
+       case 4:     
+            console.log('radan dan');
+        break;
+    default:
+        console.log('Loasa vrednost');
+}   
+*/
+      
+    //  let x = 1768;
+    // if ((x % 100 != 0 ) && (x % 4 == 0) || (x % 400 == 0)) 
+    // {
+    //     console.log ('Prestupna godina');
+    // }
+
+    //  let broj = 512;
+    //  let broj1 = broj % 10;
+    //  let broj2 = Math.floor (broj / 10) % 10;
+    //  let broj3 = Math.floor (broj /100) % 10;
+    //  console.log(broj1,broj2,broj3);
+    //  if (broj == broj1 **3 + broj2 **3 + broj3 **3) {
+    //      console.log('Armstrongov broj');
+    //  } 
+    //  else 
+    //  {
+    //     console.log('Nije Armstrongov broj');
+
+    //  }
+
+    let znak = 'sqrt';
+    let br1 = 102;
+    let br2 = 33.2;
+    switch(znak) {
+             case '+':
+            console.log(br1 + br2);
+            break;
+            case '-':
+            console.log(br1 - br2);
+            break;                
+            case '+':
+            console.log(br1 + br2);
+            break;
+            case '/':
+            console.log(br1 / br2);
+            break;
+            case '*':
+            console.log(br1 * br2);
+            break;
+            case 'sqrt':
+            console.log(Math.sqrt(br1,));
+            break;
+            case 'pow':
+            console.log(Math.pow(br1,));
+            break;
+
+            default:
+                console.log('znak ne valja');
+    }
+    /*let x= 11;
+    let y= 7;
+    switch(x){
+        case 1:
+            console.log(1);
+            break;
+            case 5:
+            if (y % 3 == 0){
+                console.log('jeste');
+            }
+            else{
+                console.log('nije');
+            }
+            break;
+            case 7: 
+            console.log('broj');
+            default:
+            console.log('nema');
+    }
+
+*/
+
+
+        
+        
+
+
+
